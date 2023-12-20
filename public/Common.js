84a93b899bd219ba2b4046032aaa9be3a9422e5f
Jordan Caldwell <jordancaldwell835@gmail.com>
2023-12-20 12:11:10 -0600

    Updated Refreshing and Main Stage Handling

--- public/Common.js
+++ public/Common.js
@@ -1,70 +1,144 @@
+let activeStreams = {}; // Global variable to keep track of active streams
+
+
 export function displayStreams(streams) {
     const streamGrid = document.getElementById('stream-grid');
-    streamGrid.innerHTML = ''; // Clear existing streams
+    const currentStreamIds = Object.keys(activeStreams);
 
-    // Sort streams: checked streams first
-    streams.sort((a, b) => {
-        const aChecked = isChecked(a.id);
-        const bChecked = isChecked(b.id);
-        return aChecked === bChecked ? 0 : aChecked ? -1 : 1;
+    if (streams.length > 0) {
+        // Find streams to remove
+        currentStreamIds.forEach(id => {
+            if (!streams.some(stream => stream.id === id)) {
+                streamGrid.removeChild(activeStreams[id]);
+                delete activeStreams[id];
+            }
+        });
+
+        // Find new streams to add
+        streams.forEach(stream => {
+            if (!activeStreams[stream.id]) {
+                const streamDiv = createStreamDiv(stream);
+                activeStreams[stream.id] = streamDiv; // Add to activeStreams
+            }
+        });
+
+        // After streams have been added to the DOM, load their checked state
+        loadCheckedState();
+
+        // Re-sort the streams in the DOM based on the checked state
+        streams.sort((a, b) => {
+            const aChecked = isChecked(a.id);
+            const bChecked = isChecked(b.id);
+            return aChecked === bChecked ? 0 : aChecked ? -1 : 1;
+        }).forEach(stream => {
+            // Append or move existing div to the correct position
+            streamGrid.appendChild(activeStreams[stream.id]);
+        });
+    }
+
+    
+
+}
+
+export function adjustStreams(streams) {
+    const streamGrid = document.getElementById('stream-grid');
+    const currentStreamIds = Object.keys(activeStreams);
+
+
+    // Find streams to remove
+    currentStreamIds.forEach(id => {
+        if (!streams.some(stream => stream.id === id)) {
+            streamGrid.removeChild(activeStreams[id]);
+            delete activeStreams[id];
+        }
     });
 
+    // Find new streams to add
     streams.forEach(stream => {
-        const streamDiv = document.createElement('div');
-        streamDiv.className = 'stream';
-        streamDiv.setAttribute('data-stream-id', stream.id);
-
-        // Controls (checkbox and button)
-        const controlsDiv = document.createElement('div');
-        controlsDiv.className = 'stream-controls';
-
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.setAttribute('data-stream-id', stream.id);
-        checkbox.checked = isChecked(stream.id);
-
-        checkbox.addEventListener('change', () => {
-            saveCheckedState(stream.id, checkbox.checked);
-
-            moveStreamDiv(stream.id, checkbox.checked);
-        });
-
-        const button = document.createElement('button');
-        button.textContent = 'Main Stage';
-        button.addEventListener('click', () => setAsMainStage(stream));
-
-        new Twitch.Embed(streamDiv, {
-            width: '100%',
-            height: '100%',
-            channel: stream.user_login, // Adjust based on your API response
-            layout: 'video',
-            muted: true,
-        });
-
-        controlsDiv.appendChild(checkbox);
-        controlsDiv.appendChild(button);
-        streamDiv.appendChild(controlsDiv);
-        streamGrid.appendChild(streamDiv);
-
-        // Page Selector
-        const pageSelector = document.getElementById('page-selector');
-    
-        // Set the current page as selected in the dropdown
-        const currentPage = window.location.pathname.split("/").pop();
-        pageSelector.value=currentPage;
-        
-        pageSelector.addEventListener('change', function() {
-        window.location.href = this.value;
-        });
+        if (!activeStreams[stream.id]) {
+            const streamDiv = createStreamDiv(stream);
+            activeStreams[stream.id] = streamDiv; // Add to activeStreams
+        }
     });
 
     // After streams have been added to the DOM, load their checked state
     loadCheckedState();
+
+    // Re-sort the streams in the DOM based on the checked state
+    streams.sort((a, b) => {
+        const aChecked = isChecked(a.id);
+        const bChecked = isChecked(b.id);
+        return aChecked === bChecked ? 0 : aChecked ? -1 : 1;
+    }).forEach(stream => {
+        // Append or move existing div to the correct position
+        streamGrid.appendChild(activeStreams[stream.id]);
+    });
+
+
+    
+
 }
+
+
+function createStreamDiv(stream) {
+    const streamDiv = document.createElement('div');
+    streamDiv.className = 'stream';
+    streamDiv.setAttribute('data-stream-id', stream.id);
+
+    // Twitch Embed
+    new Twitch.Embed(streamDiv, {
+        width: '100%',
+        height: '100%',
+        channel: stream.user_login, // Adjust based on your API response
+        layout: 'video',
+        muted: true,
+    });
+
+    // Stream Controls (e.g., checkbox, button)
+    const controlsDiv = document.createElement('div');
+    controlsDiv.className = 'stream-controls';
+
+    // Add checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.setAttribute('data-stream-id', stream.id);
+    checkbox.checked = isChecked(stream.id);
+    checkbox.addEventListener('change', () => {
+        saveCheckedState(stream.id, checkbox.checked);
+        moveStreamDiv(stream.id, checkbox.checked);
+    });
+    controlsDiv.appendChild(checkbox);
+
+    // Add button
+    const button = document.createElement('button');
+    button.textContent = 'Main Stage';
+    button.addEventListener('click', () => setAsMainStage(stream));
+    controlsDiv.appendChild(button);
+
+    // Append controls to streamDiv
+    streamDiv.appendChild(controlsDiv);
+
+    return streamDiv;
+}
+
+
+let currentMainStageStream = null; // Global variable to track the current main stage stream
 
 export function setAsMainStage(stream) {
     const mainStageContainer = document.getElementById('main-stage');
     const mainStreamContainer = document.getElementById('main-stream');
+    const streamGrid = document.getElementById('stream-grid');
+
+    // Handle existing main stage stream
+    if (currentMainStageStream && currentMainStageStream.id !== stream.id) {
+        // Re-add the existing main stage stream to the grid
+        const existingMainStreamDiv = createStreamDiv(currentMainStageStream);
+        streamGrid.appendChild(existingMainStreamDiv);
+        activeStreams[currentMainStageStream.id] = existingMainStreamDiv;
+    }
+
+    // Set new main stage stream
+    currentMainStageStream = stream; // Update the current main stage stream
     mainStreamContainer.innerHTML = ''; // Clear the current main stream
 
     // Create and add the new main stream embed
@@ -77,9 +151,18 @@ export function setAsMainStage(stream) {
     });
     mainStreamContainer.appendChild(newMainStreamDiv);
 
+    // Remove the new main stage stream from the grid
+    const streamDiv = document.querySelector(`div[data-stream-id="${stream.id}"]`);
+    if (streamDiv) {
+        streamGrid.removeChild(streamDiv);
+        delete activeStreams[stream.id]; // Update the active streams list
+    }
+
     // Update visibility of the main stage
     toggleMainStageVisibility(mainStreamContainer);
 }
+
+
 
 export function toggleMainStageVisibility(mainStreamContainer) {
     const mainStageContainer = document.getElementById('main-stage');
@@ -163,5 +246,20 @@ export function setHideButtonListener(){
 
         // Run the toggleMainStageVisibility function
         toggleMainStageVisibility(mainStreamContainer);
+    });
+
+    document.getElementById('refresh-streams').addEventListener('click', async function(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch('/api/streams');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const streams = await response.json();
+            adjustStreams(streams);
+        } catch (error) {
+            console.error('Error refreshing streams:', error);
+            // Handle error (e.g., show a message to the user)
+        }
     });
 }
