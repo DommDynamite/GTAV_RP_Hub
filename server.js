1bbddd75c6cfb9cd6c17134744a4a55ed7eb1935
Jordan <jordancaldwell835@gmail.com>
2023-12-31 11:26:33 -0600

    Website Working

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 
 // Route for the main page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/onx.html');
 });
 
 // OAuth callback route
